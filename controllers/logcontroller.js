f71df1736d7d6fcdeeaae1fe436531b08a792177
Chris Rumler <cgrumler@gmail.com>
2020-10-19 22:05:01 -0400

    update to logcontroller.js

--- controllers/logcontroller.js
+++ controllers/logcontroller.js
@@ -14,7 +14,7 @@ router.get('/log', (req, res) => {
     res.send("Log GET Test successful")
 });
 
-//Log POST endpoint (a.k.a. where I hit my roadblock that I could not overcome in time)
+//Log POST endpoint (a.k.a. where I hit my roadblock that I could not overcome in time...posting via Postman results in "column "description" of relation "logs" does not exist"".....but why does it say "logs" and not "log"?  Where is it getting "logs" plural?  I've done an entire search of my server-side code for the word "logs", but came up with nothing.)
 router.post('/log', (req, res) => {
     //res.send('here is the post')
     var describeField = req.body.log.description;
@@ -22,15 +22,25 @@ router.post('/log', (req, res) => {
     var theResult = req.body.log.result;
     //var theOwner = req.body.log.owner_id;
 
-    LogModel
-        .create({
+    LogModel.create({
             description: describeField,
             definition: define,
             result: theResult
             //owner_id: theOwner
-        }).then(dataFromDatabase => {
-            res.send('Data submitted successfully!')
-        })
+    }).then(
+        function createSuccess(log) {
+            res.json({
+                log: log,
+                message: 'created'
+            });
+        },
+        function createError(err) {
+            res.send(500, err.message);
+        }
+    );
+        // }).then(dataFromDatabase => {
+        //     res.send('Data submitted successfully!')
+        // })
 });
 
 module.exports = router;
